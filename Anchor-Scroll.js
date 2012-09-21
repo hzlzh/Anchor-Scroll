46c3ddd69aaefc5f41b98ad28e0e8eda99b6c890
hzlzh <hzlzh.dev@gmail.com>
2012-09-21 23:14:54 +0800

    fix one # bug

--- Anchor-Scroll.js
+++ Anchor-Scroll.js
@@ -1,23 +1,25 @@
 // Load jQuery.js firstly
 jQuery(function() {
-  jQuery('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var $target = jQuery(this.hash);
-      var $url = this.hash.slice(1);
-      var $scrollTime = 500;
+    jQuery('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = jQuery(this.hash);
+            var $url = this.hash.slice(1);
+            var $scrollTime = 500;
 
-      function updateUrl() {
-        window.location.hash = encodeURIComponent($url);
-      }
-      $target = $target.length && $target || jQuery('[name=' + $url + ']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        jQuery('html,body').animate({
-          scrollTop: targetOffset
-        }, $scrollTime);
-        setTimeout(updateUrl, $scrollTime + 100)
-        return false;
-      }
-    }
-  });
+            function updateUrl() {
+                window.location.hash = encodeURIComponent($url);
+            }
+            $target = $target.length && $target || jQuery('[name=' + $url + ']');
+            if (!$url) {
+                return false;
+            } else if ($target.length) {
+                var targetOffset = $target.offset().top;
+                jQuery('html,body').animate({
+                    scrollTop: targetOffset
+                }, $scrollTime);
+                setTimeout(updateUrl, $scrollTime + 100)
+                return false;
+            }
+        }
+    });
 });
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Anchor-Scroll
 =============
 
-Scroll the anchor smoothly and change the browser's **URL** at the same time.
+Scroll the anchor smoothly and **change** the browser's **URL** at the same time.
 
+<<<<<<< HEAD
 See live Demo [here](http://jsbin.com/Anchor-Scroll/latest 'Anchor-Scroll demo')
+=======
+See Demo [here](http://hzlzh.github.com/Anchor-Scroll/demo.html 'Anchor-Scroll demo') & [Fiddle code](http://jsfiddle.net/hzlzh/d44kt/ 'Fiddle code')
+>>>>>>> fix one # bug
